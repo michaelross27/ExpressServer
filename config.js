229656471ea0933c4582059cdae2c70367f49dd2
Michael Ross <michaelross27@gmail.com>
2021-03-24 22:28:30 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '276233193977975',
+        clientSecret: '6ca813bbe77e06810c309da6b04ecbb9'
+    }
 }
